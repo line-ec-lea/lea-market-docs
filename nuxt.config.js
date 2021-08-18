42499ce476fff631d561a205aed4acb023c9a74b
yusei-hyodo <yusei.hyodo@youlib.co.jp>
2021-08-19 05:29:36 +0900

    :wrench: Update Nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,27 +1,13 @@
 import theme from '@nuxt/content-theme-docs'
 
-const themeColor = '#efdca6'
+const accentColor = '#ff6600'
 
 export default theme({
   docs: {
-    primaryColor: themeColor
+    primaryColor: accentColor,
   },
 
-  head: {
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
-  },
-
-  loading: { color: themeColor },
-  generate: {
-    fallback: '404.html', // for Netlify
-    routes: ['/'] // give the first url to start crawling
-  },
+  loading: { color: accentColor },
 
   i18n: {
     locales: () => [{
@@ -31,16 +17,5 @@ export default theme({
       name: '日本語'
     }],
     defaultLocale: 'ja'
-  },
-
-  // build
-  build: {
-    babel: {
-      plugins: [
-        ['@babel/plugin-proposal-class-properties', { loose: true }],
-        ['@babel/plugin-proposal-private-methods', { loose: true }],
-        ['@babel/plugin-proposal-private-property-in-object', { loose: true }]
-      ]
-    },
   }
 })
